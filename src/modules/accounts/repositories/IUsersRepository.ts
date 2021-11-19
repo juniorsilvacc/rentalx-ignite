cd643a4fc6d7e383cd2492b6eb7d4fcbdcfe4799
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-11-19 18:23:09 -0300

    authentication on routes

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -10,6 +10,8 @@ class IUsersRepository {
   }: ICreateUserDTO): Promise<void>;
 
   findByEmail(email: string): Promise<User>;
+
+  findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
