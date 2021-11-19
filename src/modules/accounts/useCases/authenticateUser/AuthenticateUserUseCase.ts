5f09b3bee6b796f265187421ceb500a933c5e66d
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-11-19 16:17:52 -0300

    user authentication with jwt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { UsersRepository } from "../../repositories/implementations/UsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Verificar se usuário existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    // Verificar se a senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect");
+    }
+
+    // Gerar o token
+    const token = sign({}, "01d94b9cca210de5c8d3fdc2d5da8c6d", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    // Retorno
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
